d12d6dd68d1577878994d84195f9975a78e9611c
mariusz <mariusz.ozdoba@apagroup.pl>
2019-02-04 12:45:23 +0100

    login form

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,13 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('base.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
